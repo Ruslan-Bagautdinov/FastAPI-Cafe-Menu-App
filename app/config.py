e5b4dd9a338bee507beb69ac2d372b4ca3ca04eb
ruslan3lodey <ruslan3lodey@gmail.com>
2024-07-03 17:30:25 +0300

    v.0.2.9 new path for img

--- app/config.py
+++ app/config.py
@@ -1,14 +1,16 @@
-from os import getenv, path
+import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-WORK_DATABASE_URL = getenv('WORK_DATABASE_URL')
-LOCAL_DATABASE_URL = getenv('LOCAL_DATABASE_URL')
+WORK_DATABASE_URL = os.getenv('WORK_DATABASE_URL')
+LOCAL_DATABASE_URL = os.getenv('LOCAL_DATABASE_URL')
 
-HOME_DB = getenv('HOME_DB', False)
+HOME_DB = os.getenv('HOME_DB', False)
 
-BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MAIN_PHOTO_FOLDER = os.path.join(BASE_DIR, 'img')
+
 
 MIME_TYPES = {
     "jpeg": "image/jpeg",
--- app/routers/get_image.py
+++ app/routers/get_image.py
@@ -8,25 +8,33 @@ from app.config import MIME_TYPES
 
 router = APIRouter()
 
-STATIC_PHOTO_FOLDER = "static/photo"
-default_avatar_path = os.path.join(STATIC_PHOTO_FOLDER, 'default_cafe_04.jpeg')
+from app.config import MAIN_PHOTO_FOLDER
+default_avatar_path = os.path.join(MAIN_PHOTO_FOLDER, 'default_cafe_04.jpeg')
 
 
 @router.get("/")
-async def get_image(path: str = Query(None, description="The image filename")):
+async def get_image(
+    restaurant_id: int = Query(None, description="The ID of the restaurant"),
+    photo: str = Query(None, description="The filename of the photo")
+):
     """
-    Retrieves an image from the static photo folder or returns a default image if the specified image is not found.
+    Retrieves a photo from the static photo folder or returns a default photo if the specified photo is not found.
 
     Args:
-        path (str): The filename of the image to retrieve. If not provided, the default image will be returned.
+        restaurant_id (int): The ID of the restaurant.
+        photo (str): The filename of the photo to retrieve. If not provided, the default photo will be returned.
 
     Returns:
-        StreamingResponse: A streaming response containing the image bytes.
+        StreamingResponse: A streaming response containing the photo bytes.
 
     Raises:
-        HTTPException: 404 error if the default image is not found.
+        HTTPException: 404 error if the default photo is not found.
     """
-    full_path = os.path.join(STATIC_PHOTO_FOLDER, path) if path else default_avatar_path
+    if restaurant_id and photo:
+        restaurant_id = str(restaurant_id)
+        full_path = os.path.join(MAIN_PHOTO_FOLDER, restaurant_id, photo)
+    else:
+        full_path = default_avatar_path
 
     if os.path.exists(full_path):
         photo_bytes = await read_photo(full_path)
@@ -36,10 +44,10 @@ async def get_image(path: str = Query(None, description="The image filename")):
         photo_bytes = await read_photo(default_avatar_path)
 
     if photo_bytes is None:
-        raise HTTPException(status_code=404, detail="Default image not found")
+        raise HTTPException(status_code=404, detail="Default photo not found")
 
     # Determine the media type based on the file extension
-    file_extension = path.split('.')[-1].lower() if path else 'jpeg'
+    file_extension = photo.split('.')[-1].lower() if photo else 'jpeg'
     media_type = MIME_TYPES.get(file_extension, "application/octet-stream")
 
     return StreamingResponse(io.BytesIO(photo_bytes), media_type=media_type)
