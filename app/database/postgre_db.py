89d4cc9eb2b12c1ef0d15a08ecbd841c52c66a5f
ruslan3lodey <ruslan3lodey@gmail.com>
2024-07-11 14:24:23 +0300

    db engine change

--- app/database/postgre_db.py
+++ app/database/postgre_db.py
@@ -1,8 +1,9 @@
+import logging
+
 from sqlalchemy.ext.asyncio import (AsyncSession,
                                     async_sessionmaker,
                                     create_async_engine)
 from sqlalchemy.orm import declarative_base
-import logging
 
 from app.config import HOME_DB, WORK_DATABASE_URL, LOCAL_DATABASE_URL
 
@@ -11,11 +12,10 @@ if HOME_DB is True:
 else:
     DATABASE_URL = WORK_DATABASE_URL
 
-
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+engine = create_async_engine(DATABASE_URL, pool_timeout=60, pool_size=250, max_overflow=50, echo=False)
 
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
@@ -24,7 +24,7 @@ Base = declarative_base()
 
 async def init_db():
     try:
-        engine = create_async_engine(DATABASE_URL, echo=False)
+        engine = create_async_engine(DATABASE_URL, pool_timeout=60, pool_size=250, max_overflow=50, echo=False)
         async with engine.begin() as conn:
             logger.debug("Creating tables...")
             await conn.run_sync(Base.metadata.create_all)
@@ -36,5 +36,6 @@ async def init_db():
 async def get_session() -> AsyncSession:
     async with async_session() as session:
         yield session
+
 
 logger.info(DATABASE_URL)
