2d52b2af11c2dc0e2f20f5a45b6255663294528d
ruslan3lodey <ruslan3lodey@gmail.com>
2024-07-12 16:25:47 +0300

    waiter call date time added 2

--- app/database/schemas.py
+++ app/database/schemas.py
@@ -94,6 +94,7 @@ class WaiterCallCreateRequest(BaseModel):
 
 class WaiterCallResponse(BaseModel):
     id: uuid.UUID = Field(..., description="Unique identifier of the waiter call")
+    call_datetime: datetime = Field(..., description="Date and time of the waiter call")
     restaurant_id: int = Field(..., description="ID of the restaurant")
     table_id: int = Field(..., description="ID of the table")
     status: str = Field(..., description="Status of the waiter call")
